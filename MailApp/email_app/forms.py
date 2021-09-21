79ee24ed66a5778a65750b470cc9d52486bab0c5
bartekche <bartekche@gmail.com>
2021-09-21 23:48:53 +0200

    Recipients in txt file feature
    Added 2 new fields to the form, file and separator.  If the file contains email addresses divided by the specified separator, the email will be send to all email addresses in the file.

--- MailApp/email_app/forms.py
+++ MailApp/email_app/forms.py
@@ -4,3 +4,5 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
+    file = forms.FileField()
+    separator = forms.CharField(max_length=1)
--- MailApp/email_app/views.py
+++ MailApp/email_app/views.py
@@ -2,19 +2,22 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from .forms import ContactForm
+from .files import handle_uploaded_file
 from django.core.mail import send_mail
 
 def get_name(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
         # check whether it's valid:
         if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
             subject = form.cleaned_data['subject']
+            sep = form.cleaned_data['separator']
             message = form.cleaned_data['message']
             sender = form.cleaned_data['sender']
-            recipients = ['bartekche@gmail.com']
+            recipients = open('temp_plik/name.txt', 'r').read().split(sep)
             recipients.append(sender)
             send_mail(subject, message, "emailapp.formularz@gmail.com", recipients)
         return HttpResponseRedirect('/thanks/')
